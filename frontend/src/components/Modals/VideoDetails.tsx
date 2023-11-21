d3fb7363428ee28dc2eb1969fbd995ae91d63426
Mian009 <100489826+Mian009@users.noreply.github.com>
2023-11-21 21:34:56 +0500

    complete detail view

--- frontend/src/components/Modals/VideoDetails.tsx
+++ frontend/src/components/Modals/VideoDetails.tsx
@@ -7,6 +7,63 @@ interface VideoDetailProps {
 }
 
 function VideoDetails({ handleCloseModal }: VideoDetailProps) {
+  const commentsData = [
+    {
+      id: "1",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "2",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "3",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "4",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "5",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+    },
+    {
+      id: "6",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "7",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "8",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+    {
+      id: "9",
+      name: "Hannery",
+      profilePicture: IMAGES.Profile,
+      comment: "very nice video",
+    },
+  ];
   const myBGStyleModal = {
     backgroundColor: "rgba(0, 0, 0, 0.6)",
     backdropFilter: "blur(8px)",
@@ -17,7 +74,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
         style={myBGStyleModal}
         className="fixed inset-0 flex items-center justify-center z-50"
       >
-        <div className="modal-container w-11/12 sm:w-11/12 lg:w-9/12 h-[40rem] lg:h-[calc(100vh - 2rem)] z-50 overflow-y-auto dark:bg-[#091619] rounded-lg">
+        <div className="modal-container w-11/12 sm:w-11/12 lg:w-9/12 h-[40rem] lg:h-[calc(100vh - 2rem)] z-50 overflow-y-auto dark:bg-[#091619] rounded-lg no-scrollbar">
           {/* Modal content */}
           <div className="relative bg-white rounded-lg dark:bg-[#091619] py-5 sm:py-4 border dark:border-[#586769]">
             <button
@@ -36,7 +93,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
               <span className="sr-only">Close modal</span>
             </button>
 
-            <div className="flex flex-col sm:flex-row sm:justify-between lg:h-[33.9rem] md:h-[33.9rem] h-[8rem] justify-center border-2 border-[#1C2C2E] rounded-lg my-8 mx-4">
+            <div className="flex flex-col sm:flex-row sm:justify-between lg:h-[33.9rem] md:h-[33.9rem] h-[8rem] justify-center border-2 border-[#1C2C2E] rounded-lg my-8 mx-4 no-scrollbar">
               {/* Left Column - Story Block */}
               <div className="w-full sm:w-1/2 sm:mr-2 sm:ml-6 border-r-2 border-[#1C2C2E]">
                 {/* Add your story block content here */}
@@ -48,9 +105,9 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
               </div>
 
               {/* Right Column - Form */}
-              <div className="w-full md:w-full lg:w-2/5 lg:h-[33.9rem] md:h-[33.9rem] h-[8rem]">
+              <div className="w-full md:w-full lg:w-7/12 lg:h-[33.9rem] md:h-[33.9rem] h-[8rem]">
                 {/* Game Selection */}
-                <div className="ml-4">
+                <div className="w-full md:w-full lg:w-full ml-3">
                   <div className="flex items-center my-3">
                     <Image
                       className="w-12 h-12 mr-2 sm:mr-4"
@@ -75,10 +132,10 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                     </p>
                   </div>
 
-                  <div className="flex items-center justify-evenly">
-                    <div className="flex justify-items-start gap-4">
+                  <div className="flex items-center w-11/12">
+                    <div className="flex flex-1 gap-4">
                       <Image
-                        className="p-2 rounded-xl bg-[#162423]"
+                        className="p-2 w-10 h-10 rounded-xl bg-[#162423]"
                         src={SVG.Like}
                         alt="Like"
                         width={50}
@@ -86,6 +143,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                       />
 
                       <Image
+                        className="w-10 h-10 rounded-xl bg-[#162423]"
                         src={SVG.Comment}
                         alt="Comment"
                         width={50}
@@ -93,6 +151,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                       />
 
                       <Image
+                        className="w-10 h-10 rounded-xl bg-[#162423]"
                         src={SVG.Trending1}
                         alt="Trending1"
                         width={50}
@@ -100,6 +159,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                       />
 
                       <Image
+                        className="p-2 w-10 h-10 rounded-xl bg-[#162423]"
                         src={SVG.Gcoin}
                         alt="Gcoin"
                         width={50}
@@ -107,12 +167,12 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                       />
                     </div>
 
-                    <div className="flex flex-col sm:flex-row justify-center sm:justify-between">
-                      <p className="text-sm ">5.4K Like</p>
+                    <div className="flex flex-1">
+                      <p className="text-sm ">5.4K Like &nbsp;</p>
                       <p className="text-sm"> . 165 Comments</p>
                     </div>
 
-                    <div>
+                    <div className="flex flex-end ">
                       <Image
                         src={SVG.Share}
                         alt="Share"
@@ -123,91 +183,40 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                   </div>
                 </div>
 
-                <hr className="w-full border-t border-gray-600 my-4" />
+                <hr className="w-full border-t border-gray-600 mt-4" />
 
-                <div className="ml-4">
-                  <div className="flex items-center mt-6">
-                    <Image
-                      className="mr-2 sm:mr-4"
-                      src={IMAGES.Profile}
-                      alt="Profile avatar"
-                      width={40}
-                      height={40}
-                    />
-                    <div>
-                      <div className="flex items-center mb-1">
-                        <p className="text-lg font-light md:text-xl dark:text-white">
-                          mark_john
-                        </p>
-                        <p className="ml-3 font-light md:text-md dark:text-gray-200">
-                          Very nice video
-                        </p>
-                      </div>
-                      <div className="flex items-center text-base font-light sm:text-sm text-gray-50 dark:text-gray-50">
-                        <p className="mr-2">1d</p>
-                        <p className="mr-2">8 likes</p>
-                        <p>Reply</p>
+                <div className="h-72 mx-4 overflow-y-scroll no-scrollbar">
+                  {commentsData.map((item) => (
+                    <div key={item.id} className="flex items-start gap-2 mt-3">
+                      <Image
+                        className="w-12 h-12"
+                        src={item.profilePicture}
+                        alt="Profile avatar"
+                        width={40}
+                        height={40}
+                      />
+                      <div className="flex flex-col">
+                        <div className="flex mb-1">
+                          <p className="text-lg font-bold md:text-lg dark:text-white">
+                            {item.name}
+                          </p>
+                        </div>
+                        <div className="flex items-start">
+                          <p className="ml-2 font-light md:text-md dark:text-gray-200">
+                            {item.comment}
+                          </p>
+                        </div>
+                        <div className="flex items-start text-base font-light sm:text-sm text-gray-50 dark:text-gray-50">
+                          <p className="mr-2">1d</p>
+                          <p className="mr-2">8 likes</p>
+                          <p>Reply</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
 
-                <div className="ml-4">
-                  <div className="flex items-center mt-6">
-                    <Image
-                      className="mr-2 sm:mr-4"
-                      src={IMAGES.Profile}
-                      alt="Profile avatar"
-                      width={40}
-                      height={40}
-                    />
-                    <div>
-                      <div className="flex items-center mb-1">
-                        <p className="text-lg font-light md:text-xl dark:text-white">
-                          mark_john
-                        </p>
-                        <p className="ml-3 font-light md:text-md dark:text-gray-200">
-                          Very nice video
-                        </p>
-                      </div>
-                      <div className="flex items-center text-base font-light sm:text-sm text-gray-50 dark:text-gray-50">
-                        <p className="mr-2">1d</p>
-                        <p className="mr-2">8 likes</p>
-                        <p>Reply</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="ml-4 mb-40">
-                  <div className="flex items-center mt-6">
-                    <Image
-                      className="mr-2 sm:mr-4"
-                      src={IMAGES.Profile}
-                      alt="Profile avatar"
-                      width={40}
-                      height={40}
-                    />
-                    <div>
-                      <div className="flex items-center mb-1">
-                        <p className="text-lg font-light md:text-xl dark:text-white">
-                          mark_john
-                        </p>
-                        <p className="ml-3 font-light md:text-md dark:text-gray-200">
-                          Very nice video
-                        </p>
-                      </div>
-                      <div className="flex items-center text-base font-light sm:text-sm text-gray-50 dark:text-gray-50">
-                        <p className="mr-2 ">1d</p>
-                        <p className="mr-2">8 likes</p>
-                        <p>Reply</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <hr className="w-full border-t border-gray-600" />
-                <form className="w-full mx-2">
+                <form className="flex w-full mx-2">
                   <div className="relative">
                     <div className="absolute inset-y-0 left-0 flex items-center  pointer-events-none">
                       <Image
@@ -220,7 +229,7 @@ function VideoDetails({ handleCloseModal }: VideoDetailProps) {
                     <input
                       type="Post"
                       id="default-search"
-                      className="w-96 block p-4 ml-10 text-sm bg-[#091619] outline-none sm:text-sm dark:text-white"
+                      className="w- block p-4 ml-10 text-sm bg-[#091619] outline-none sm:text-sm dark:text-white"
                       placeholder="Add a comment..."
                       required
                     />
