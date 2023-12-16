79123ea2e9f0a572d029807b8f447c0664b3946c
“Hamza <hamzashaikhwork@gmail.com>
2023-12-16 11:23:25 +0500

    feat: update

--- frontend/src/font/font.ts
+++ frontend/src/font/font.ts
@@ -1,4 +1,4 @@
 import { League_Gothic, Lexend_Exa } from "next/font/google";
 
-export const leagueGothic = League_Gothic({ subsets: ["latin"] });
-export const lexendExa = Lexend_Exa({ subsets: ["latin"] });
+export const leagueGothic = League_Gothic({ subsets: ["latin"], preload: false, display: "swap", adjustFontFallback: false });
+export const lexendExa = Lexend_Exa({ subsets: ["latin"],  preload: false, display: "swap", adjustFontFallback: false });
