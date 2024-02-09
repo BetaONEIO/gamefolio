96d1078bcee1e16cfaf80c4a19ff73cd365d7402
“Hamza <hamzashaikhwork@gmail.com>
2024-02-09 20:46:00 +0500

    feat: V2- edit profile added

--- frontend/src/app/account/settings/edit-profile/page.tsx
+++ frontend/src/app/account/settings/edit-profile/page.tsx
@@ -69,6 +69,8 @@ const EditProfile = () => {
   // Watch the value of the 'accountType' field
   const accountTypeValue = watch("accountType");
 
+  console.log("authState", authState);
+
   useEffect(() => {
     onUpdate(authState);
   }, [authState]);
@@ -124,9 +126,12 @@ const EditProfile = () => {
     <Setting>
       {/* Header */}
 
-      <div className="flex justify-start px-6 py-8 md:h-screen lg:py-0">
-        <div className="flex relative p-6 sm:p-8">
+      <div className="flex flex-col justify-center px-6 py-8  lg:py-0">
+        <div className="relative py-4">
           <div className="mb-4 w-20 h-20 rounded-lg relative">
+            {/* <span className="block font-bold mb-2 text-sm text-white">
+              Edit Profile
+            </span> */}
             <Image
               src={
                 image ? URL.createObjectURL(image) : authState.profilePicture
@@ -157,15 +162,131 @@ const EditProfile = () => {
           </div>
         </div>
 
-        <div className="p-6 space-y-4 sm:p-8 md:w-96">
-          <div className="text-md text-center text-gray-50">
+        <div className="flex w-full ">
+          <form
+            className="flex w-full flex-wrap gap-2 justify-start"
+            onSubmit={handleSubmit(handleUpdateProfile)}
+          >
+            <div className="flex-1 basis-full lg:basis-5/12 ">
+              <label
+                htmlFor="name"
+                className="block font-bold mb-2 text-sm text-white"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id="name"
+                className="bg-[#162423] sm:text-sm outline-none rounded-lg block w-full p-2.5 text-white"
+                placeholder="Name"
+                required
+                {...register("name", {
+                  required: true,
+                  max: 12,
+                  min: 4,
+                  maxLength: 12,
+                })}
+              />
+            </div>
+            <div className="flex-1 basis-full lg:basis-5/12 ">
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-bold text-white"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="bg-[#162423] sm:text-sm outline-none rounded-lg block w-full p-2.5 text-white"
+                placeholder="Username"
+                required
+                {...register("username", {
+                  required: true,
+                  max: 16,
+                  min: 4,
+                  maxLength: 16,
+                })}
+              />
+            </div>
+
+            <div className="flex-1 basis-full lg:basis-5/12 ">
+              <label
+                htmlFor="Date of Birth"
+                className="block mb-2 text-sm font-bold text-white"
+              >
+                Date of Birth (optional)
+              </label>
+              <input
+                type="Date"
+                className="bg-[#162423] sm:text-sm rounded-lg outline-none block w-full p-2.5 text-white hover:opacity-80 cursor-pointer"
+                placeholder="Date of Birth"
+                {...register("dateOfBirth")}
+              />
+            </div>
+            <div className="flex-1 basis-full lg:basis-5/12 ">
+              <p className="mb-2 text-sm font-bold text-gray-50">
+                Account Type
+              </p>
+              <div className="flex justify-center w-full gap-2">
+                <button
+                  type="button"
+                  onClick={() => onUpdateAccountType("public")}
+                  className={
+                    accountTypeValue === "public"
+                      ? "text-white w-full justify-center bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#162423] border-green-500 border"
+                      : "w-full justify-center text-gray-500 items-center outline-none rounded-lg text-sm font-medium px-5 py-2.5 bg-[#162423]"
+                  }
+                >
+                  Public
+                </button>
+                <button
+                  type="button"
+                  onClick={() => onUpdateAccountType("private")}
+                  className={
+                    accountTypeValue === "private"
+                      ? "text-white w-full justify-center bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-[#162423] border-green-500 border"
+                      : "w-full justify-center text-gray-500 items-center outline-none rounded-lg text-sm font-medium px-5 py-2.5 bg-[#162423]"
+                  }
+                >
+                  Private
+                </button>
+              </div>
+            </div>
+            <div className="basis-6/12  mb-4">
+              <label className="block mb-2 text-sm font-bold text-white">
+                Bio
+              </label>
+              <textarea
+                id="description"
+                rows={3}
+                className="bg-[#162423] sm:text-sm rounded-lg outline-none block w-full p-2.5 text-white"
+                placeholder="Lorem ipsum dolor sit amet consectetur. Ante duis tellus tincidunt nibh"
+                {...register("bio", {
+                  required: true,
+                  min: 10,
+                  maxLength: 100,
+                })}
+              ></textarea>
+            </div>
+
+            <div className="w-full flex justify-center lg:justify-end gap-4">
+              <span className="w-6/12 lg:w-4/12 font-bold h-12 bg-[#586769] text-white text-center rounded-tl-[20px] rounded-br-[20px] rounded-tr-[5px] rounded-bl-[5px] mb-3 flex justify-center items-center">
+                Cancel
+              </span>
+              <button className="w-6/12 lg:w-4/12 font-bold h-12 bg-[#37C535] text-white text-center rounded-tl-[20px] rounded-br-[20px] rounded-tr-[5px] rounded-bl-[5px] mb-3">
+                Update
+              </button>
+            </div>
+          </form>
+          {/* <div className="text-md text-center text-gray-50">
             <p
               className="font-bold text-primary-600 underline hover:opacity-80 cursor-pointer"
               onClick={() => handleModalToggle("isDeleteModalOpen")}
             >
               Deactivate Account
             </p>
-          </div>
+          </div> */}
         </div>
       </div>
       <ToastContainer
