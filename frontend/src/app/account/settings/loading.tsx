de32662d0ac47ab11056f26306de6ba2303c5273
“Hamza <hamzashaikhwork@gmail.com>
2024-02-12 15:29:37 +0500

    feat: V2 - Feedback, Block Users modal added and loader fixed

--- frontend/src/app/account/settings/loading.tsx
+++ frontend/src/app/account/settings/loading.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Setting from "./page";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-[#DB2777] h-12 w-12"></div>
-    </div>
+    <Setting>
+      <div className="flex items-center justify-center h-full ">
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-[#DB2777] h-12 w-12"></div>
+      </div>
+    </Setting>
   );
 }
--- frontend/src/app/account/settings/page.tsx
+++ frontend/src/app/account/settings/page.tsx
@@ -1,20 +1,17 @@
 "use client";
-import React, { Suspense, useState } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import { SVG } from "@/assets/SVG";
+import CustomHeader from "@/components/CustomHeader/CustomHeader";
 import Layout from "@/components/CustomLayout/layout";
 import BlockUser from "@/components/Modals/BlockUser";
-import ChangePassword from "@/components/Modals/ChangePassword";
 import FeedBack from "@/components/Modals/FeedBack";
 import LogOut from "@/components/Modals/LogOut";
 import Modal from "@/components/Modals/Modal";
-import { leagueGothic } from "@/font/font";
-import Loading from "./loading";
 import { toastError } from "@/components/Toast/Toast";
-import { ToastContainer } from "react-toastify";
-import CustomHeader from "@/components/CustomHeader/CustomHeader";
+import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { ToastContainer } from "react-toastify";
 
 function Setting({ children }: any) {
   const [modalState, setModalState] = useState({
@@ -52,220 +49,226 @@ function Setting({ children }: any) {
 
   return (
     <Layout>
-      <Suspense fallback={<Loading />}>
-        {/* Header */}
-        <CustomHeader>SETTINGS</CustomHeader>
-
-        {/* Body */}
-        <div style={sectionStyle} className="h-screen w-full p-8">
-          <div className="flex flex-col md:flex-row bg-[#091619] h-5/6 w-full   border border-[#1C2C2E] rounded-lg p-6   ">
-            <div className="flex md:flex-col gap-2 bg-[#091619] overflow-y-scroll no-scrollbar">
-              {/* Box 1 */}
-              <Link href="/account/settings/edit-profile">
-                <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                  <div className="flex flex-col md:flex-row  gap-2 justify-start items-center h-full">
-                    <Image
-                      className="text-green-500 fill-current"
-                      src={SVG.Profile}
-                      alt="Edit Profile"
-                      width={42}
-                      height={42}
-                    />
-                    <span
-                      className={`text-xs text-center md:text-base font-semibold ${
-                        isItemActive("/account/settings/edit-profile")
-                          ? "text-[#43DD4E]"
-                          : "text-white"
-                      }`}
-                    >
-                      Edit Profile
-                    </span>
-                  </div>
+      {/* Header */}
+      <CustomHeader>SETTINGS</CustomHeader>
+      {/* Body */}
+      <div style={sectionStyle} className="h-screen w-full p-8">
+        <div className="flex flex-col md:flex-row bg-[#091619] h-5/6 w-full   border border-[#1C2C2E] rounded-lg p-6   ">
+          <div className="flex md:flex-col gap-2 bg-[#091619] overflow-y-scroll no-scrollbar">
+            {/* Box 1 */}
+            <Link href="/account/settings/edit-profile">
+              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
+                <div className="flex flex-col md:flex-row  gap-2 justify-start items-center h-full">
+                  <Image
+                    className="text-green-500 fill-current"
+                    src={SVG.Profile}
+                    alt="Edit Profile"
+                    width={42}
+                    height={42}
+                  />
+                  <span
+                    className={`text-xs text-center md:text-base font-semibold ${
+                      isItemActive("/account/settings/edit-profile")
+                        ? "text-[#43DD4E]"
+                        : "text-white"
+                    }`}
+                  >
+                    Edit Profile
+                  </span>
                 </div>
-              </Link>
+              </div>
+            </Link>
 
-              {/* Box 2 */}
-              <Link href="/account/settings/change-password">
-                <div
-                  className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl"
-                  // onClick={() => handleModalToggle("isChangePasswordOpen")}
+            {/* Box 2 */}
+            <Link href="/account/settings/change-password">
+              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
+                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                  <Image
+                    src={SVG.ChangePassword}
+                    alt="Change Password"
+                    width={42}
+                    height={42}
+                  />
+                  <span
+                    className={`text-xs text-center md:text-base font-semibold ${
+                      isItemActive("/account/settings/change-password")
+                        ? "text-[#43DD4E]"
+                        : "text-white"
+                    }`}
+                  >
+                    Change Password
+                  </span>
+                </div>
+              </div>
+            </Link>
+            {/* Box 3 */}
+            <Link href="/account/settings/terms-condition">
+              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
+                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                  <Image
+                    src={SVG.TermCondition}
+                    alt="Edit Profile"
+                    width={42}
+                    height={42}
+                  />
+                  <span
+                    className={`text-xs text-center md:text-base font-semibold ${
+                      isItemActive("/account/settings/terms-condition")
+                        ? "text-[#43DD4E]"
+                        : "text-white"
+                    }`}
+                  >
+                    Terms & Conditions
+                  </span>
+                </div>
+              </div>
+            </Link>
+            {/* Box 4 */}
+            <Link href="/account/settings/privacy-policy">
+              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
+                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                  <Image
+                    src={SVG.PrivacyPolicy}
+                    alt=" Privacy Policy"
+                    width={42}
+                    height={42}
+                  />
+                  <span
+                    className={`text-xs text-center md:text-base font-semibold ${
+                      isItemActive("/account/settings/privacy-policy")
+                        ? "text-[#43DD4E]"
+                        : "text-white"
+                    }`}
+                  >
+                    Privacy Policy
+                  </span>
+                </div>
+              </div>
+            </Link>
+            {/* Box 5 */}
+            <Link href="/account/settings/verification">
+              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
+                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                  <Image
+                    src={SVG.Verification}
+                    alt="Verifications"
+                    width={42}
+                    height={42}
+                  />
+                  <span
+                    className={`text-xs text-center md:text-base font-semibold ${
+                      isItemActive("/account/settings/verification")
+                        ? "text-[#43DD4E]"
+                        : "text-white"
+                    }`}
+                  >
+                    Verifications
+                  </span>
+                </div>
+              </div>
+            </Link>
+            {/* Box 6 */}
+            <div
+              className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl"
+              onClick={() => handleModalToggle("isSendFeedbackModalOpen")}
+            >
+              <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                <Image
+                  src={SVG.SendFeedback}
+                  alt="Send Feedback"
+                  width={42}
+                  height={42}
+                />
+                <span
+                  className={`text-xs text-center md:text-base font-semibold ${
+                    modalState.isSendFeedbackModalOpen
+                      ? "text-[#43DD4E]"
+                      : "text-white"
+                  }`}
                 >
-                  <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                    <Image
-                      src={SVG.ChangePassword}
-                      alt="Change Password"
-                      width={42}
-                      height={42}
-                    />
-                    <span
-                      className={`text-xs text-center md:text-base font-semibold ${
-                        isItemActive("/account/settings/change-password")
-                          ? "text-[#43DD4E]"
-                          : "text-white"
-                      }`}
-                    >
-                      Change Password
-                    </span>
-                  </div>
-                </div>
-              </Link>
-              {/* Box 3 */}
-              <Link href="/account/settings/terms-condition">
-                <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                  <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                    <Image
-                      src={SVG.TermCondition}
-                      alt="Edit Profile"
-                      width={42}
-                      height={42}
-                    />
-                    <span
-                      className={`text-xs text-center md:text-base font-semibold ${
-                        isItemActive("/account/settings/terms-condition")
-                          ? "text-[#43DD4E]"
-                          : "text-white"
-                      }`}
-                    >
-                      Terms & Conditions
-                    </span>
-                  </div>
-                </div>
-              </Link>
-              {/* Box 4 */}
-              <Link href="/account/settings/privacy-policy">
-                <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                  <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                    <Image
-                      src={SVG.PrivacyPolicy}
-                      alt=" Privacy Policy"
-                      width={42}
-                      height={42}
-                    />
-                    <span
-                      className={`text-xs text-center md:text-base font-semibold ${
-                        isItemActive("/account/settings/privacy-policy")
-                          ? "text-[#43DD4E]"
-                          : "text-white"
-                      }`}
-                    >
-                      Privacy Policy
-                    </span>
-                  </div>
-                </div>
-              </Link>
-              {/* Box 5 */}
-              <Link href="/account/settings/verification">
-                <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                  <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                    <Image
-                      src={SVG.Verification}
-                      alt="Verifications"
-                      width={42}
-                      height={42}
-                    />
-                    <span
-                      className={`text-xs text-center md:text-base font-semibold ${
-                        isItemActive("/account/settings/verification")
-                          ? "text-[#43DD4E]"
-                          : "text-white"
-                      }`}
-                    >
-                      Verifications
-                    </span>
-                  </div>
-                </div>
-              </Link>
-              {/* Box 6 */}
-              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                  <Image
-                    src={SVG.SendFeedback}
-                    alt="Send Feedback"
-                    width={42}
-                    height={42}
-                  />
-                  <span className="text-xs text-center md:text-base font-semibold text-white">
-                    Send Feedback
-                  </span>
-                </div>
-              </div>
-              {/* Box 7 */}
-              <div className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl">
-                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                  <Image
-                    src={SVG.BlockUser}
-                    alt="Block Users"
-                    width={42}
-                    height={42}
-                  />
-                  <span className="text-xs text-center md:text-base font-semibold text-white">
-                    Block Users
-                  </span>
-                </div>
-              </div>
-              {/* Box 8 */}
-              <div
-                className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl"
-                onClick={() => handleModalToggle("isLogoutModalOpen")}
-              >
-                <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
-                  <Image src={SVG.Logout} alt="Logout" width={42} height={42} />
-                  <span className="text-xs text-center md:text-base font-semibold text-white">
-                    Logout
-                  </span>
-                </div>
+                  Send Feedback
+                </span>
               </div>
             </div>
-            <hr className="w-full md:h-full md:w-0 border m-2 bg-gray-300 opacity-10" />
-            {/* Dynamic Content */}
-            <div className=" flex-1 overflow-y-scroll no-scrollbar">
-              {children}
+            {/* Box 7 */}
+            <div
+              className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl"
+              onClick={() => handleModalToggle("isBlockUserModalOpen")}
+            >
+              <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                <Image
+                  src={SVG.BlockUser}
+                  alt="Block Users"
+                  width={42}
+                  height={42}
+                />
+                <span
+                  className={`text-xs text-center md:text-base font-semibold ${
+                    modalState.isBlockUserModalOpen
+                      ? "text-[#43DD4E]"
+                      : "text-white"
+                  }`}
+                >
+                  Block Users
+                </span>
+              </div>
+            </div>
+            {/* Box 8 */}
+            <div
+              className="hover:opacity-80 cursor-pointer bg-[#091619] h-fit p-4 rounded-xl"
+              onClick={() => handleModalToggle("isLogoutModalOpen")}
+            >
+              <div className="flex flex-col md:flex-row gap-2 justify-start items-center h-full">
+                <Image src={SVG.Logout} alt="Logout" width={42} height={42} />
+                <span
+                  className={`text-xs text-center md:text-base font-semibold ${
+                    modalState.isLogoutModalOpen
+                      ? "text-[#43DD4E]"
+                      : "text-white"
+                  }`}
+                >
+                  Logout
+                </span>
+              </div>
             </div>
           </div>
+          <hr className="w-full md:h-full md:w-0 border m-2 bg-gray-300 opacity-10" />
+          {/* Dynamic Content */}
+          <div className=" flex-1 overflow-y-scroll no-scrollbar">
+            {children}
+          </div>
         </div>
+      </div>
 
-        {/* Modals */}
+      {/* Modals */}
 
-        <Modal
-          isOpen={modalState.isChangePasswordOpen}
-          handleClose={() => handleModalToggle("isChangePasswordOpen")}
-        >
-          <ChangePassword
-            handleCloseModal={(error?: string) =>
-              handleModalToggle("isChangePasswordOpen", error)
-            }
-          />
-        </Modal>
+      <Modal
+        isOpen={modalState.isSendFeedbackModalOpen}
+        handleClose={() => handleModalToggle("isSendFeedbackModalOpen")}
+      >
+        <FeedBack
+          handleCloseModal={() => handleModalToggle("isSendFeedbackModalOpen")}
+        />
+      </Modal>
 
-        <Modal
-          isOpen={modalState.isSendFeedbackModalOpen}
-          handleClose={() => handleModalToggle("isSendFeedbackModalOpen")}
-        >
-          <FeedBack
-            handleCloseModal={() =>
-              handleModalToggle("isSendFeedbackModalOpen")
-            }
-          />
-        </Modal>
+      <Modal
+        isOpen={modalState.isBlockUserModalOpen}
+        handleClose={() => handleModalToggle("isBlockUserModalOpen")}
+      >
+        <BlockUser
+          handleCloseModal={() => handleModalToggle("isBlockUserModalOpen")}
+        />
+      </Modal>
 
-        <Modal
-          isOpen={modalState.isBlockUserModalOpen}
-          handleClose={() => handleModalToggle("isBlockUserModalOpen")}
-        >
-          <BlockUser
-            handleCloseModal={() => handleModalToggle("isBlockUserModalOpen")}
-          />
-        </Modal>
+      <Modal
+        isOpen={modalState.isLogoutModalOpen}
+        handleClose={() => handleModalToggle("isLogoutModalOpen")}
+      >
+        <LogOut
+          handleCloseModal={() => handleModalToggle("isLogoutModalOpen")}
+        />
+      </Modal>
 
-        <Modal
-          isOpen={modalState.isLogoutModalOpen}
-          handleClose={() => handleModalToggle("isLogoutModalOpen")}
-        >
-          <LogOut
-            handleCloseModal={() => handleModalToggle("isLogoutModalOpen")}
-          />
-        </Modal>
-      </Suspense>
       <ToastContainer
         position="top-center"
         autoClose={5000}
