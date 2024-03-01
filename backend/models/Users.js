9268148eb8eb51025fed6c6de69960573716bdc1
“Hamza <hamzashaikhwork@gmail.com>
2024-03-01 16:20:33 +0500

    feat: Registration date added

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -124,6 +124,10 @@ const userSchema = new mongoose.Schema({
   forgotOTP: {
     type: String,
   },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 // hash user's password with salt before saving document to db
