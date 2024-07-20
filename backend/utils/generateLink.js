b3527e12eb05ced69a62f9cb5ae04cf4c0d14727
“Hamza <hamzashaikhwork@gmail.com>
2024-07-20 20:21:01 +0500

    google & twitter callback url fixed

--- backend/utils/generateLink.js
+++ backend/utils/generateLink.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 function generateUniqueLink() {
   const uniqueID = uuidv4();
   const hash = crypto.createHash("sha256").update(uniqueID).digest("hex");
-  return `${process.env.BASE_URL}/main?v=${hash}`;
+  return `${process.env.WEB_URL}/main?v=${hash}`;
 }
 
 module.exports = generateUniqueLink;
